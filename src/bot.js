552ca003611fd3ca83d1f617a8690bba8570f9a0
Rodrigo Perrucci Macharelli <rmacharelli@masterdom.inf.br>
2020-08-25 05:46:08 -0300

    Adicionado tts

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const fs = require('fs');
 const express = require('express');
-
+const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1.js');
+const { IamAuthenticator } = require('ibm-watson/auth');
 const Discord = require('discord.js');
+const { Client } = require('pg');
+
 const client = new Discord.Client();
 
-const { Client } = require('pg');
 const dbclient = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -20,15 +22,53 @@ let frases = [];
 dbclient.query(`SELECT * FROM frases`, (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
-        console.log(row.frase);
         frases.push(row.frase);
     }
 });
 
+const tts_client = new TextToSpeechV1({
+    authenticator: new IamAuthenticator({
+        apikey: process.env.WATSON_TTS_APIKEY,
+    }),
+    url: "https://stream.watsonplatform.net/text-to-speech/api",
+});
+
 function respondRandomPhrase(msg) {
     let frase = frases[Math.floor(Math.random() * frases.length)];
-    msg.reply(`Frases Yuri: \n\n${frase}`);
+    msg.reply(`Frases Yuri DEV: \n\n${frase}`);
     msg.react('514865275473100800');
+    playTranscription(msg, frase)
+}
+
+function playTranscription(msg, frase) {
+    var voiceChannel = msg.member.voice.channel;
+    if (voiceChannel) {
+        let params = {
+            text: frase,
+            voice: 'pt-BR_IsabelaV3Voice',
+            accept: 'audio/mp3'
+        }
+
+        let data = [];
+        tts_client.synthesize(params)
+            .then(response => {
+                let audio = response.result;
+
+                var write_stream = fs.createWriteStream("output.mp3");
+                audio.pipe(write_stream);
+
+                audio.on('end', () => {
+                    console.log("data")
+                    voiceChannel.join().then(connection => {
+                        const dispatcher = connection.play('output.mp3');
+                        dispatcher.on("finish", end => {
+                            voiceChannel.leave();
+                        });
+                    }).catch(err => console.log(err));
+                })
+            })
+            .catch(console.error);
+    }
 }
 
 function addPhrase(new_phrase) {
